13205c1842684ac68c1f1beb7de5daf7ee8f91c0
ryym <ryym.64@gmail.com>
2016-11-01 00:07:37 +0900

    chore(env): Enable to run npm scripts on localhost
    Processes on Docker containers are slow..

--- config/env.js
+++ config/env.js
@@ -1,10 +1,35 @@
 /**
- * Export environment variables.
+ * Load environment variables.
+ * Although we can run processes both on localhost
+ * and a Docker container, some processes may not run
+ * correctly because there are node module(s) that have
+ * a difference build process for platforms (Darwin or Linux).
  */
 
 const process = require('process');
+const path = require('path');
+const fs = require('fs');
+const dotenv = require('dotenv');
 
-const envVars = process.env;
+function loadEnvVars(envVars, isDocker, nodeEnv) {
+
+  // If in a Docker container, all environemnt variables
+  // must be already set by docker-compose file.
+  if (isDocker) {
+    return envVars;
+  }
+
+  const envFilePath = path.join(__dirname, 'envs', `${nodeEnv}.env`);
+  const envVarsInFile = dotenv.parse(fs.readFileSync(envFilePath));
+  return Object.assign(envVarsInFile, envVars);
+}
+
+const envVars = loadEnvVars(
+  process.env,
+  Boolean(process.env.DOCKER),
+  process.env.NODE_ENV || 'dev'
+);
+
 const env = {
   DB_PASSWORD: envVars.POSTGRES_PASSWORD,
   DB_USER: envVars.POSTGRES_USER,

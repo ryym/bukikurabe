9f145a69d5f497ae29c60bcb2228c8981b1158b6
ryym <ryym.64@gmail.com>
2016-11-01 22:22:56 +0900

    feat(db): Add default value to some columns for testability
    デフォルト値がないと、テストでモックデータを作る時にいちいち
    全ての値を指定しないといけないため。
    本当はマイグレーションファイルを変更するのはダメだけど、まだ
    最初なので良し。

--- db/migrations/201610192238_create_tables.sql
+++ db/migrations/201610192238_create_tables.sql
@@ -12,21 +12,21 @@ CREATE TABLE meta_data (
 CREATE TABLE sub_weapons (
   id SMALLSERIAL PRIMARY KEY,
   name VARCHAR(16) NOT NULL,
-  description VARCHAR(160) NOT NULL
+  description VARCHAR(160) NOT NULL DEFAULT ''
 );
 
 CREATE TABLE special_weapons (
   id SMALLSERIAL PRIMARY KEY,
   name VARCHAR(16) NOT NULL,
-  description VARCHAR(160) NOT NULL
+  description VARCHAR(160) NOT NULL DEFAULT ''
 );
 
 CREATE TABLE weapon_types (
   id SMALLSERIAL PRIMARY KEY,
   name VARCHAR(10) NOT NULL,
-  spec1_name VARCHAR(10) NOT NULL,
-  spec2_name VARCHAR(10) NOT NULL,
-  spec3_name VARCHAR(10) NOT NULL
+  spec1_name VARCHAR(10) NOT NULL DEFAULT '',
+  spec2_name VARCHAR(10) NOT NULL DEFAULT '',
+  spec3_name VARCHAR(10) NOT NULL DEFAULT ''
 );
 
 CREATE TYPE special_diminution AS ENUM (
@@ -38,13 +38,13 @@ CREATE TYPE special_diminution AS ENUM (
 CREATE TABLE main_weapons (
   id SMALLSERIAL PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
-  description VARCHAR(200) NOT NULL,
+  description VARCHAR(200) NOT NULL DEFAULT '',
   weapon_type_id SMALLINT NOT NULL REFERENCES weapon_types,
   spec1_value SMALLINT NOT NULL DEFAULT 0,
   spec2_value SMALLINT NOT NULL DEFAULT 0,
   spec3_value SMALLINT NOT NULL DEFAULT 0,
   sub_weapon_id SMALLINT NOT NULL REFERENCES sub_weapons,
   special_weapon_id SMALLINT NOT NULL REFERENCES special_weapons,
-  special_diminution special_diminution NOT NULL,
+  special_diminution special_diminution NOT NULL DEFAULT 'small',
   price INTEGER NOT NULL DEFAULT 0
 );
